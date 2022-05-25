3eeca2a6c1aebd4296abf38599d3fc639132654d
Israel Abraham <israelvictory87@gmail.com>
2022-05-25 21:32:26 +0100

    Set SOTP_MILLI_SECONDS to 15 minutes (ms)

--- core/settings.py
+++ core/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# SOTP configuration
+SOTP_MILLI_SECONDS = 960000 # 15 minutes
