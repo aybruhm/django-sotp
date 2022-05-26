61ab2277f1d99b0381f23365a5f43f5dfe4861db
Israel Abraham <israelvictory87@gmail.com>
2022-05-26 21:53:23 +0100

    Created remove_otps and schedulers under helpers

---
+++ sotp/helpers/remove_otps
@@ -0,0 +1,16 @@
+# Django Imports
+from django.contrib.auth import get_user_model
+
+# SOTP Imports
+from sotp.services import GenerateSOTP
+
+
+User = get_user_model()
+sotp = GenerateSOTP()
+
+
+def remove_user_sotps(user:User):
+    
+    pass
+
+
---
+++ sotp/helpers/schedulers.py
@@ -0,0 +1,32 @@
+# ApScheduler Imports
+from apscheduler.schedulers.background import BackgroundScheduler
+from django_apscheduler.jobstores import DjangoJobStore
+
+# Native Imports
+import sys
+
+# Django Imports
+from django.contrib.auth import get_user_model
+
+# SOTP Helpers Imports
+from sotp.helpers.remove_otps import remove_user_sotps
+
+
+User = get_user_model()
+
+
+def run_scheduler(user:User):
+    scheduler = BackgroundScheduler()
+    scheduler.add_jobstore(DjangoJobStore(), "default")
+    
+    # run remove user totps and otps job every 15 minutes
+    scheduler.add_job(
+        remove_user_sotps, 
+        'interval', minutes=15,
+        jobstore='default',
+        id="{}_totp_{}_otp_{}".format(user.username, user.totp, user.otp),
+    )
+
+    # Start scheduler
+    scheduler.start()
+    print("Scheduler started...", file=sys.stdout)
