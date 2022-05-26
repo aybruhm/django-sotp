fa214c3fda52072c058e1534a78e9a3e73e628f9
Israel Abraham <israelvictory87@gmail.com>
2022-05-26 21:23:12 +0100

    Created UserSOTP model

--- sotp/models.py
+++ sotp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import get_user_model
+from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
@@ -7,13 +7,14 @@ User = get_user_model()
 
 class UserSOTP(User):
     """Inherits the user model class to extend functionalities"""
-    totp = models.CharField(unique=True, editable=False, null=True, blank=True)
+    totp = models.CharField(max_length=18, unique=True, editable=False, null=True, blank=True)
     otp = models.IntegerField(max_length=6, editable=False, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=False)
     
     def __str__(self):
         return "totp: {}, otp: {}".format(self.totp, self.otp)
     
     class Meta:
-        abstract = True
         verbose_name_plural = "User SOTPs"
         db_table = "user_sotps"
