e1027d7ddeaaca77b99cda85954ceb2489a76b22
Israel Abraham <israelvictory87@gmail.com>
2022-05-28 20:31:40 +0100

    Update remove_user_sopts function

--- sotp/helpers/remove_otps.py
+++ sotp/helpers/remove_otps.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 # SOTP Imports
 from sotp.services import GenerateSOTP
+from sotp.models import UserSOTP
 
 
 User = get_user_model()
@@ -10,7 +11,17 @@ sotp = GenerateSOTP()
 
 
 def remove_user_sotps(user:User):
+    """
+    It removes the user's SOTP.
     
-    pass
+    :param user: The user object
+    :type user: User
+    :return: True
+    """
+    user_sotp = UserSOTP.objects.get(id=user.id)
+    user_sotp.totp = ""
+    user_sotp.otp = ""
+    user_sotp.save()
+    return True
 
 
