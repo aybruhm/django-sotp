6793c47a25b687eea68285dd8bd48144a1b1d315
Israel Abraham <israelvictory87@gmail.com>
2022-05-30 21:44:11 +0100

    Created example app

--- djangorest_sotp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangorestSotpConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'djangorest_sotp'
--- example/apps.py
+++ example/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DjangoSotpConfig(AppConfig):
+class ExampleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_sotp'
+    name = 'example'
