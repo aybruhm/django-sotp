fd73145ae8549958575cc0bde9120e284cd80e89
Israel Abraham <israelvictory87@gmail.com>
2022-05-30 21:37:21 +0100

    Update run_scheduler function

--- sotp/helpers/schedulers.py
+++ sotp/helpers/schedulers.py
@@ -4,27 +4,28 @@ from django_apscheduler.jobstores import DjangoJobStore
 
 # Native Imports
 import sys
+import random
 
 # Django Imports
 from django.contrib.auth import get_user_model
 
 # SOTP Helpers Imports
-from sotp.helpers.remove_otps import remove_user_sotps
+from sotp.helpers.remove_otps import remove_user_otp
 
 
 User = get_user_model()
 
 
-def run_scheduler(user:User):
+def run_scheduler(user_id:int):
     scheduler = BackgroundScheduler()
     scheduler.add_jobstore(DjangoJobStore(), "default")
     
     # run remove user totps and otps job every 15 minutes
     scheduler.add_job(
-        remove_user_sotps, 
+        remove_user_otp, 
         'interval', minutes=15,
         jobstore='default',
-        id="#{}_totp_{}_otp_{}".format(user.id, user.totp, user.otp),
+        id="#{}-{}_remove_elapsed_otps".format(user_id, random.randint(00000000, 9000000)),
     )
 
     # Start scheduler
