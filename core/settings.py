3b5e38e0cc274db423a9a20f623935b1f302812e
Israel Abraham <israelvictory87@gmail.com>
2022-05-30 21:48:08 +0100

    Created example app

--- core/settings.py
+++ core/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'django_sotp',
-    'djangorest_sotp',
+    'example',
     'sotp',
     
     'django_apscheduler',
