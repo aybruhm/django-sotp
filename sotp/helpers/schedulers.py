ece16b9c34c6ed5fcd8ad5cd2c8a2b75bf0aadc1
Israel Abraham <israelvictory87@gmail.com>
2022-05-31 07:42:36 +0100

    Added sotp time expiration in scheduler

--- sotp/helpers/schedulers.py
+++ sotp/helpers/schedulers.py
@@ -8,6 +8,7 @@ import random
 
 # Django Imports
 from django.contrib.auth import get_user_model
+from django.conf import settings
 
 # SOTP Helpers Imports
 from sotp.helpers.remove_otps import remove_user_otp
@@ -23,7 +24,7 @@ def run_scheduler(user_id:int):
     # run remove user totps and otps job every 15 minutes
     scheduler.add_job(
         remove_user_otp, 
-        'interval', minutes=15,
+        'interval', minutes=settings.SOTP_TIME_EXPIRATION,
         jobstore='default',
         id="#{}-{}_remove_elapsed_otps".format(user_id, random.randint(00000000, 9000000)),
     )
