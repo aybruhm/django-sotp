56024c4dfd21f60444d0de6a7041912802d3c626
Israel Abraham <israelvictory87@gmail.com>
2022-06-05 17:47:54 +0100

    Created logout url path

--- example/urls.py
+++ example/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from example.views import redirect_to_register_page, register_page, \
-    login_page, confirm_otp_page, welcome_page
+    login_page, confirm_otp_page, welcome_page, logout_user
 
 
 app_name = "example"
@@ -11,5 +11,6 @@ urlpatterns = [
     path("register/", register_page, name="register-page"),
     path("login/", login_page, name="login-page"),
     path("confirm-otp/", confirm_otp_page, name="confirm-otp-page"),
-    path("welcome/", welcome_page, name="welcome-page")
+    path("welcome/", welcome_page, name="welcome-page"),
+    path("logout/", logout_user, name="logout_user")
 ]
