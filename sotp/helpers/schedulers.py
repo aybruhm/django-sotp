804450dab89611aba55bc5274ddb5dd6a5f5fee4
Israel Abraham <israelvictory87@gmail.com>
2022-06-07 13:01:50 +0100

    Refactor run_scheduler

--- sotp/helpers/schedulers.py
+++ sotp/helpers/schedulers.py
@@ -10,7 +10,7 @@ import random
 from django.conf import settings
 
 # SOTP Helpers Imports
-from sotp.helpers.remove_otps import remove_user_otp
+from sotp.helpers.remove_otps import remove_user_otp_and_job
 
 
 def run_scheduler(user_email:str):
@@ -22,16 +22,20 @@ def run_scheduler(user_email:str):
     # Register event
     register_events(scheduler)
     
+    # Generate Job ID
+    job_id = "{}-{}_remove_elapsed_otps"\
+            .format(user_email, random.randint(00000000, 9000000))
+    
     # run remove user totps and otps job every 15 minutes
     scheduler.add_job(
-        remove_user_otp, 
+        remove_user_otp_and_job, 
         'interval', minutes=settings.SOTP_TIME_EXPIRATION,
         jobstore='default',
-        id="{}-{}_remove_elapsed_otps"\
-            .format(user_email, random.randint(00000000, 9000000)),
+        id=job_id, max_instances=1,
         replace_existing=True,
+        kwargs={"email": user_email, "job_id": job_id}
     )
 
     # Start scheduler
-    scheduler.start()
+    scheduler.start() 
     print("Scheduler started...", file=sys.stdout)
