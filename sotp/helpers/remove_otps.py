c1e3258256f0969e42fab0d3649a0481cf2f731d
Israel Abraham <israelvictory87@gmail.com>
2022-06-07 13:02:24 +0100

    Remove user job from database after scheduler runs

--- sotp/helpers/remove_otps.py
+++ sotp/helpers/remove_otps.py
@@ -4,11 +4,11 @@ from django.contrib.auth import get_user_model
 # SOTP Imports
 from sotp.models import UserSOTP
 
+# Third Party Imports
+from django_apscheduler.models import DjangoJob
 
-User = get_user_model()
 
-
-def remove_user_otp(**kwargs):
+def remove_user_otp_and_job(**kwargs):
     """
     It removes the OTP from the database 
     for the user with the email address passed 
@@ -17,13 +17,16 @@ def remove_user_otp(**kwargs):
     :return: A boolean value.
     """
     user_sotp = UserSOTP.objects.get(
-        user=User.objects.get(
+        user=get_user_model().objects.get(
             email=kwargs.get("email")
         )
     )
     user_sotp.totp = 0
     user_sotp.otp = 0
     user_sotp.save()
+    
+    # Removes user job from database
+    DjangoJob.objects.get(id=kwargs.get("job_id")).delete()
     
     return True
 
