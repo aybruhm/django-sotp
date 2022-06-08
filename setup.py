35e34e0cb5dbf06e67a962f9ca48e4a4441e06cd
Israel Abraham <israelvictory87@gmail.com>
2022-06-08 22:50:36 +0100

    Bump version 1.0.1 -> 1.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
  
 setup(
     name = 'django_sotp',
-    version = '1.0.1',
+    version = '1.0.2',
     packages = find_packages(),
     include_package_data = True,
     license = 'CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
